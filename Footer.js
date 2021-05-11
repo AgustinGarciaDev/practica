f735042847d8f39803c1e6b66d272d3d0225b4dc
Agustin Garcia <agustinchinchilla16@gmail.com>
2021-05-11 10:54:13 -0300

    cambios

--- Footer.js
+++ Footer.js
@@ -1,26 +1,26 @@
 import React from 'react'
 
 
-const Footer=()=>{
-    return(
- <div>
-    <div>   
-        <div  className="footer">
-            <div className="social">
-                <img src="../assets/fb.png" alt="facebook" className="img-social"/>
-                <img src="../assets/inst.png" alt="instangram" className="img-social"/>
-                <img src="../assets/wsp.png" alt="whatsapp" className="img-social"/>
-            </div>
+const Footer = () => {
+    return (
+        <div>
             <div>
-                <p>ACA LA INFOOO JAJAJAJA</p>
+                <div className="estoEsUncONFLITO">
+                    <div className="social">
+                        <img src="../assets/fb.png" alt="facebook" className="img-social" />
+                        <img src="../assets/inst.png" alt="instangram" className="img-social" />
+                        <img src="../assets/wsp.png" alt="whatsapp" className="img-social" />
+                    </div>
+                    <div>
+                        <p>ACA LA INFOOO JAJAJAJA</p>
+                    </div>
+
+                </div>
+                <div >
+                    <p>CHOCLAZO</p>
+                </div>
             </div>
-        
         </div>
-            <div >
-              <p>CHOCLAZO</p>
-            </div>
-     </div>
-</div>
     )
 }
 export default Footer
