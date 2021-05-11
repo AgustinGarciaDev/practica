2ad33a4f2ceeadaa3f76bcdd3e36e5d5e5cd0c29
camila <camiladomato@gmail.com>
2021-05-11 10:31:19 -0300

    el footer

---
+++ Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const Footer=()=>{
+    return(
+ <div>
+    <div>   
+        <div  className="footer">
+            <div className="social">
+                <img src="../assets/fb.png" alt="facebook" className="img-social"/>
+                <img src="../assets/inst.png" alt="instangram" className="img-social"/>
+                <img src="../assets/wsp.png" alt="whatsapp" className="img-social"/>
+            </div>
+        
+        </div>
+            <div >
+              <p>CHOCLAZO</p>
+            </div>
+     </div>
+</div>
+    )
+}
+export default Footer
